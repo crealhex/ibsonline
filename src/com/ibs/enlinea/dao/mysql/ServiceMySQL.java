a32321bdcef47ce569cf3a7a2ec41a43abdfff65
Luis Enco <38085121+crealhex@users.noreply.github.com>
2020-03-01 01:36:48 -0500

    ServiceMySQL.java base file created

---
+++ src/com/ibs/enlinea/dao/mysql/ServiceMySQL.java
@@ -0,0 +1,33 @@
+package com.ibs.enlinea.dao.mysql;
+
+import com.ibs.enlinea.dao.interfaces.ServiceDAO;
+import com.ibs.enlinea.models.Service;
+
+import java.util.List;
+
+public class ServiceMySQL implements ServiceDAO {
+    @Override
+    public void create(Service object) {
+        
+    }
+
+    @Override
+    public List<Service> read() {
+        return null;
+    }
+
+    @Override
+    public void update(Service object) {
+
+    }
+
+    @Override
+    public void delete(Service object) {
+
+    }
+
+    @Override
+    public Service getById(Integer integer) {
+        return null;
+    }
+}
